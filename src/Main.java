02014e1720649af9c37456ba3ac1f3785eee559f
nielsperdon <niels.perdon@gmail.com>
2022-11-23 17:18:11 +0100

    Main method testing

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         MyLinkedList list = new MyLinkedList(null);
         list.traverse(list.getRoot());
-        String stringData = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
+        String stringData = "5 7 3 9 8 2 1 0 4 6";
 
         String[] data = stringData.split(" ");
         for (String s : data){
@@ -11,5 +11,6 @@ public class Main {
 
         }
         list.traverse(list.getRoot());
+
     }
 }
