de20d5455a1911cdf13bcea8be31bf1f8a5c047e
nielsperdon <niels.perdon@gmail.com>
2022-11-23 17:55:25 +0100

    adding SearchThree.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,51 @@
 public class Main {
     public static void main(String[] args) {
+        SearchThree tree = new SearchThree(null);
+        tree.traverse(tree.getRoot());
 
-        MyLinkedList list = new MyLinkedList(null);
-        list.traverse(list.getRoot());
-        String stringData = "5 7 3 9 8 2 1 0 4 6";
+       // String stringData = "9 1 8 2 7 3 6 4 5";
+        String stringData = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
 
         String[] data = stringData.split(" ");
-        for (String s : data){
-           list.addItem(new Node(s));
-
+        for (String s: data){
+            tree.addItem(new Node(s));
         }
-        list.traverse(list.getRoot());
+
+        tree.traverse(tree.getRoot());
+
+
+//        MyLinkedList list = new MyLinkedList(null);
+//        list.traverse(list.getRoot());
+//        String stringData = "5 7 3 9 8 2 1 0 4 6";
+//
+//        String[] data = stringData.split(" ");
+//        for (String s : data){
+//           list.addItem(new Node(s));
+//
+//        }
+//        list.traverse(list.getRoot());
+//        list.removeItem(new Node("3"));
+//        list.traverse(list.getRoot());
+//
+//        list.removeItem(new Node("5"));
+//        list.traverse(list.getRoot());
+//
+//
+//        list.removeItem(new Node("0"));
+//        list.removeItem(new Node("4"));
+//        list.removeItem(new Node("2"));
+//        list.traverse(list.getRoot());
+//
+//        list.removeItem(new Node("9"));
+//        list.traverse(list.getRoot());
+//        list.removeItem(new Node("8"));
+//        list.traverse(list.getRoot());
+//        list.removeItem(new Node("6"));
+//        list.traverse(list.getRoot());
+//        list.removeItem(list.getRoot()); // removing first item in the list
+//        list.traverse(list.getRoot());
+//        list.removeItem(list.getRoot());
+//        list.traverse(list.getRoot());
 
     }
 }
