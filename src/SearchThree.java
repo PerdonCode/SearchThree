7084af7b81f45d4be0c511f729bc273b1633b229
nielsperdon <niels.perdon@gmail.com>
2022-11-23 18:15:21 +0100

    adding method

--- src/SearchThree.java
+++ src/SearchThree.java
@@ -50,7 +50,76 @@ public class SearchThree implements NodeList {
 
     @Override
     public boolean removeItem(ListItem item) {
+        if (item != null) {
+            System.out.println("Deleting item " + item.getValue());
+        }
+        ListItem currentItem = this.root;
+        ListItem parentItem = currentItem;
+
+        while (currentItem != null) {
+            int comparison = (currentItem.compareTo(item));
+            if (comparison < 0) {
+                parentItem = currentItem;
+                currentItem = currentItem.next();
+            } else if (comparison > 0) {
+                parentItem = currentItem;
+                currentItem = currentItem.previous();
+            } else {
+                performRemoval(currentItem, parentItem);
+                return true;
+            }
+        }
         return false;
+    }
+
+    private void performRemoval(ListItem item, ListItem parent) {
+        // remove item from the three
+        if (item.next() == null) {
+            // no right thee, so make parent point to the left tree
+            if (parent.next() == item) {
+                // item is right child of its parent
+                parent.setNext(item.previous());
+            } else if (parent.previous() == item) {
+                // item is left child of its parent
+                parent.setPrevious(item.previous());
+            } else {
+                // parent must be item, which means we are looking at the root of the tree
+                this.root = item;
+            }
+        } else if (item.previous() == null) {
+            // no left tree, so make point to right tree
+            if (parent.next() == item) {
+                // item is right child of its parent
+                parent.setNext(item.next());
+            } else if (parent.previous() == item) {
+                // item is left child of its parent
+                parent.setPrevious(item.next());
+            } else {
+                // again, we are deleting the root
+                this.root = item.next();
+            }
+        }else {
+            // neither left nor right are null
+            // from the right sub-tree find the smallest value
+            ListItem current = item.next();
+            ListItem leftmostParent = item;
+            while (current.previous() != null){
+                leftmostParent = current;
+                current = current.previous();
+            }
+            // now put the smallest value into our node to be deleted
+            item.setValue(current.getValue());
+            // and delete the smallest
+            if (leftmostParent == item){
+                // there was no leftmost node, so current points to the smallest
+                // node must be now be deleted
+                item.setNext(current.next());
+            } else {
+                // set the smallest node's parent to point to
+                // the smallest node's right child
+                leftmostParent.setPrevious(current.next());
+            }
+        }
     }
 
     @Override
